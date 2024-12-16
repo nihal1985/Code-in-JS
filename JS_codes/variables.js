177cddbba8d6566596953114fe12db2898585a1b
Nihal Singh <72004439+nihal1985@users.noreply.github.com>
2024-12-16 17:05:53 +0000

    get to know about differnt types of variables

---
+++ JS_codes/variables.js
@@ -0,0 +1,10 @@
+// Today we are gonna learn about let, const and var
+
+// Something you store, you need a memory space.
+
+let name = "Nihal" // We use let to declear a num, string, char, etc.
+var rollNo = 23 // var is same as let but don't use var as it gets buggy and the compilor sometimes make irresistable error understanding it
+
+const iD = "abc123" // constant variables are those whose values can't be modified later on after declaration.
+
+console.log(`My name is ${name}, roll no is ${rollNo}, and my personal id is ${iD}.`)
